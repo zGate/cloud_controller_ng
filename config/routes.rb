16c5e546ca14ea8936634e02f760a50f96c28106
Phan Le <ple@pivotallabs.com>
2014-01-21 10:58:30 -0800

    make sure that rake routes and rails c can load
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 CloudController::Application.routes.draw do
-  get   "rails",   to: proc { |env| [200, {}, ['From Rails!']] }
-  match "(*path)", to: Rails.application.sinatra_cc_app, via: [:get, :post, :delete, :put, :patch]
+  get "rails", to: proc { |env| [200, {}, ['From Rails!']] }
+
+  # controller and action are for rake routes and rails c
+  match "(*path)", {
+    to: Rails.application.sinatra_cc_app,
+    via: [:get, :post, :delete, :put, :patch],
+    controller: 'sinatra_cc',
+    action: 'sinatra_cc',
+  }
 end
