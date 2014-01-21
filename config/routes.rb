c06681f1c6f88c2ea78bf2cb2c19d7d63010f71b
Phan Le <ple@pivotallabs.com>
2014-01-21 10:11:09 -0800

    route all of verbs to sinatra cc app
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 CloudController::Application.routes.draw do
-  get "rails", :to => proc { |env| [200, {}, ['From Rails!']] }
-  get '(*path)', :to => Rails.application.sinatra_cc_app
+  get   "rails",   to: proc { |env| [200, {}, ['From Rails!']] }
+  match "(*path)", to: Rails.application.sinatra_cc_app, via: [:get, :post, :delete, :put, :patch]
 end
