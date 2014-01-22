affc094c613c4caf1c3893dc9073e62aac5777a7
Phan Le <ple@pivotallabs.com>
2014-01-21 17:44:08 -0800

    make sure that newrelic works with rails
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- spec/integration/performance_monitoring_spec.rb
+++ spec/integration/performance_monitoring_spec.rb
@@ -4,61 +4,58 @@ require "thread"
 
 describe "Cloud controller", type: :integration, monitoring: true do
   context "when configured to use development mode" do
-    let(:port) { 8181 }
-
-    let (:newrelic_config_file) {
-      File.expand_path(File.join(File.dirname(__FILE__), "../fixtures/config/newrelic.yml"))
-    }
-
-    let(:base_cc_config_file) {
-      "spec/fixtures/config/port_8181_config.yml"
-    }
+    port = 8181
+    fixture_cc_config_path = "spec/fixtures/config/port_8181_config.yml"
+    fixture_newrelic_config_path = "spec/fixtures/config/newrelic.yml"
 
     before do
       start_nats(debug: false)
-      opts = {
+      start_cc(
         debug: true,
-        config: cc_config_file.path,
+        config: fixture_cc_config_path,
         env: {
-          "NRCONFIG" => newrelic_config_file,
-          "RACK_ENV" => "development"
+          "NRCONFIG" => newrelic_config_file.path,
+          "RACK_ENV" => "development",
         }
-      }
-      start_cc(opts)
+      )
     end
 
     after do
       stop_cc
       stop_nats
-      cc_config_file.unlink
+      newrelic_config_file.unlink
     end
 
     context "when developer mode is enabled" do
-      let(:cc_config_file) do
-        cc_config = YAML.load_file(base_cc_config_file)
-        cc_config['development_mode'] = true
-        file = Tempfile.new("cc_config.yml")
-        file.write(YAML.dump(cc_config))
+      let(:newrelic_config_file) do
+        newrelic_config = YAML.load_file(fixture_newrelic_config_path)
+        newrelic_config['development']['developer_mode'] = true
+        file = Tempfile.new("newrelic.yml")
+        file.write(YAML.dump(newrelic_config))
         file.close
         file
       end
 
-      it "reports the transaction information in /newrelic" do
+      it "reports the transaction information in /newrelic about Rails and Sinatra endpoints" do
         info_response = make_get_request("/info", {}, port)
         expect(info_response.code).to eq("200")
+
+        rails_response = make_get_request("/rails", {}, port)
+        expect(rails_response.code).to eq("200")
 
         newrelic_response = make_get_request("/newrelic", {}, port)
         expect(newrelic_response.code).to eq("200")
         expect(newrelic_response.body).to include("/info")
+        expect(newrelic_response.body).to include("/rails")
       end
     end
 
     context "when developer mode is not enabled" do
-      let(:cc_config_file) do
-        cc_config = YAML.load_file(base_cc_config_file)
-        cc_config['development_mode'] = false
-        file = Tempfile.new("cc_config.yml")
-        file.write(YAML.dump(cc_config))
+      let(:newrelic_config_file) do
+        newrelic_config = YAML.load_file(fixture_newrelic_config_path)
+        newrelic_config['development']['developer_mode'] = false
+        file = Tempfile.new("newrelic.yml")
+        file.write(YAML.dump(newrelic_config))
         file.close
         file
       end
