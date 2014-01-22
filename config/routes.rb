00689b515b2b7ffcbfdafffa6f07d5dfacc0501a
Phan Le <ple@pivotallabs.com>
2014-01-21 17:44:20 -0800

    only load sinatra app if it is set
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 CloudController::Application.routes.draw do
   get "rails", to: "rails#index"
 
-  unless app = Rails.application.sinatra_cc_app
-    raise ArgumentError, "sinatra_cc_app must not be nil"
+  if app = Rails.application.sinatra_cc_app
+    match "(*path)", to: app, via: :all
   end
-
-  # controller and action are for rake routes and rails c
-  match "(*path)", {
-    to: app,
-    via: :all,
-    controller: 'sinatra_cc',
-    action: 'sinatra_cc',
-  }
 end
