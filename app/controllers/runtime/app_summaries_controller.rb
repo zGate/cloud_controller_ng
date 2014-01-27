3bdb5b3f0e509c0988708850a564cb1c81e7b466
Phan Le <ple@pivotallabs.com>
2014-01-27 13:34:17 -0800

    wip
    Signed-off-by: Dmitriy Kalinin <dmitriy@pivotallabs.com>

--- app/controllers/runtime/app_summaries_controller.rb
+++ app/controllers/runtime/app_summaries_controller.rb
@@ -8,3 +8,37 @@ module VCAP::CloudController
     end
   end
 end
+
+__END__
+
+module VCAP::CloudController
+  class InfoController < ApiController
+    skip_before_filter :require_identity!, only: :show
+
+    def show
+      info = Info.new(@config)
+      authorization.authorize!(:info, info)
+      render json: InfoPresenter.new(info).to_hash
+    end
+  end
+end
+
+module VCAP::CloudController
+  class BuildpackController < ApiController
+    def index
+      authorization.authorize!(:index, Buildpack)
+      buildpacks = Buildpack.all # do authed query
+      render json: CollectionPresenter.new(buildpacks).to_hash
+
+      # BuildpackPresent would actually make a DB query
+      # @renderer.render_collection(buildpacks)
+    end
+
+    def create
+      buildpack = Buildpack.new(BuildpackParams.new(params).to_hash)
+      authorization.authorize!(:create, buildpack)
+      buildpack.create!
+      render json: BuildpackPresenter.new(buildpack).to_hash
+    end
+  end
+end
