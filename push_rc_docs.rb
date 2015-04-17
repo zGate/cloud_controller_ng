b3d14507923add2ddc506d92bc417351088a3b8e
Greg Cobb and Raina Masand <gcobb@pivotal.io>
2015-04-17 09:38:35 -0700

    Script to push api_docs to release-candidate bucket
    - Based on current commit sha
    
    [#92385918]

---
+++ push_rc_docs.rb
@@ -0,0 +1,40 @@
+require 'net/http'
+require 'json'
+require 'fog'
+
+def push_docs_to_s3(travis_build_id)
+  storage = Fog::Storage.new(
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  )
+
+  docs_bucket_name = "cc-api-docs"
+  rc_dir_name = "release-candidate"
+
+  puts 'Copying API docs to release-candidate bucket'
+  source_docs = storage.directories.get(docs_bucket_name, prefix: travis_build_id)
+puts source_docs
+  source_docs.files.each do |file|
+    new_file_key = file.key.gsub(/#{travis_build_id}/, rc_dir_name)
+    file.copy(docs_bucket_name, new_file_key)
+  end
+end
+
+commit = `git rev-parse HEAD`.strip
+puts "cloud_controller_ng commit:#{commit}"
+
+uri = URI("https://api.travis-ci.org/repos/cloudfoundry/cloud_controller_ng/builds")
+result = Net::HTTP.get(uri)
+
+builds = JSON.parse(result)
+builds.select! do |build|
+  build['commit'] == commit
+end
+
+unless builds.empty?
+  travis_build_id = builds.first['id']
+  puts "Travis build id: #{travis_build_id}"
+end
+
+push_docs_to_s3(travis_build_id)
