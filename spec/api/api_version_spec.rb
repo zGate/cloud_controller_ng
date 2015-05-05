b0a0d3069ded97a74488d51b1ae814c70183f001
Joseph Palermo and Zach Robinson <zrobinson@pivotal.io>
2015-05-04 17:25:52 -0700

    Add api example response for v3 app
    [#82631320]

--- spec/api/api_version_spec.rb
+++ spec/api/api_version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'digest/sha1'
 
 describe 'Stable API warning system', api_version_check: true do
-  API_FOLDER_CHECKSUM = '7665f50de858f45bb1b219ff29a9f4212cf1aa10'
+  API_FOLDER_CHECKSUM = 'ca8e32d8c99da508a88f9c616f83181b105a91f3'
 
   it 'double-checks the version' do
     expect(VCAP::CloudController::Constants::API_VERSION).to eq('2.25.0')
